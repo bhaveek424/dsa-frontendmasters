96758f6d7f6d7aacb47d9a27cdd1265a75f67307
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-08-22 17:58:02 +0530

    Stack and Queue

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>
+    private tail?: Node<T>
 
     
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0
     }
 
-    enqueue(item: T): void {
+    enqueue(item: T): void { // we're gonna go to the tail this time
+   const node =  {value: item} as Node<T>
+        this.length++;
+        if(!this.tail) {
+            this.tail = this.head = node
+        }
+
+        this.tail.next = node
+        this.tail = node
+
 
 }
-    deque(): T | undefined {
+    deque(): T | undefined { // first we'll check if we have a head 
+        if(!this.head) {
+            return undefined
+        }
 
+        this.length-- //book-keeping
+
+        const head = this.head // save the head
+        this.head = this.head.next // update the head
+
+        //free
+        head.next = undefined
+
+        return head.value // return the value
 }
-    peek(): T | undefined {
-
+    peek(): T | undefined { // its the first value
+        return this.head?.value
 }
 }
--- src/day2/Stack.ts
+++ src/day2/Stack.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>, // it helps in visualisation of a stack as its pointing backwards
+}
 export default class Stack<T> {
     public length: number;
+    private head?: Node<T>
 
     
 
     constructor() {
+        this.head = undefined
+        this.length = 0
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>
+
+        this.length++
+        if (!this.head) {  // when there's no head
+            this.head = node
+            return;
+        }
+        // if there's a head
+        node.prev = this.head //  node is pointing to head F -> E (if we want to push F)
+        this.head = node; // head is poitint to node
 
 }
     pop(): T | undefined {
+        // we cant simply decrement our length(bookkeeping) cuz we are going to -1 territory
+        this.length = Math.max(0, this.length -1) //this ensures we stay at 0 or we countdown
+        if(this.length === 0) {
+            const head = this.head 
+            this.head = undefined
+            return head?.value
+        }
+        // to detatch 
+        const head = this.head as Node<T> // We save out the value that points to F
+        this.head = head.prev
+
+        return head.value
 
 }
     peek(): T | undefined {
+        return this.head?.value
 
 }
 }
